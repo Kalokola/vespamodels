6953669de7a0e8fd3a4546637319c5cabcc62a1b
kalokola <brightiuskalokola@gmail.com>
2021-09-15 08:34:23 +0300

    lkj

--- model_project/settings.py
+++ model_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ng8q#b_@p_)53y0&a3%2281az9#ycsu*(u$d2uf2_6!_7i*e_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
