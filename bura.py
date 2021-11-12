c87dd45581606323f511b1a0355d600fcfc9d7f7
Brightius Kalokola <69508559+drkalokola@users.noreply.github.com>
2021-11-12 23:28:43 +0300

    Delete bura.py

--- bura.py
+++
@@ -1,6 +0,0 @@
-import requests
-response = requests.get("http://api.open-notify.org/astros.json")
-print(response.json())
-#response.content() # Return the raw bytes of the data payload
-#response.text() # Return a string representation of the data payload
-response.json()
